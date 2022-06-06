cf4f5e16bd30a7d8374b72eecee147283a96f591
voatcodes <valarietila@gmail.com>
2022-06-06 18:56:28 +0300

    added functionality to create busines  profile

--- app.js
+++ app.js
@@ -134,14 +134,146 @@ app.post('/signup', (req, res) => {
 
 
 
+//display login business profile form
+
+app.get('/login-business-profile', (req, res) => {
+    const user = {
+        b_name: '',
+        b_password: '',
+    }
+    res.render('login-business-profile', {error: false, user:user})
+})
+
+
+
+//submit login business profile form
+
+app.post('/login-business-profile', (req, res) => {
+
+    connection.query(
+        'SELECT * FROM business_profile WHERE b_name = ?',
+        [req.body.b_name],
+        (error, results) => {
+            if(results.length > 0) {
+                // authenticate 
+                bcrypt.compare(req.body.b_password, results[0].b_password, (error, matches) => {
+                    if(matches) {
+                        res.redirect('/')
+                    } else {
+                        const user = {
+                            b_name: req.body.b_name,
+                            b_password: req.body.b_password
+                        }
+                        let message = 'Name/Password mismatch.'
+                        res.render('login-business-profile', {error: true, message: message, user: user})
+                    }
+                })    
+            } else {
+                const user = {
+                    b_name: req.body.b_name,
+                    b_password: req.body.b_password
+                }
+                let message = 'Account does not exist. Please create one.'
+                res.render('login-business-profile', {error: true, message: message, user: user})
+            }
+        }
+    )
+})
+
+
+
 app.get('/about', (req, res) => {
     res.render('about', {name: name})
 })
 // create a route '/about' and send 'about page' as response
 
 //create business profile
+// app.get('/create-business-profile', (req,res) => {
+//     res.render('create-business-profile')
+// })
+
+
+//display create business profile form
+
 app.get('/create-business-profile', (req,res) => {
-    res.render('create-business-profile')
+    const user = {
+        b_name: '',
+        b_tag_line: '',
+        b_category: '',
+        b_description: '',
+        b_location: '',
+        b_contact_person: '',
+        b_phone_number: '',
+        b_password: '',
+        confirmPassword: ''
+    }
+    res.render('create-business-profile', {error: false, user: user})
+})
+
+//submit create business profile form
+
+app.post('/create-business-profile', (req,res) => {
+
+    const profile = {
+        business: {
+                name: req.body.businessName,
+                email: req.body.businessEmail,
+                tagLine:req.body.tagLine,
+                category: req.body.businessCategory,
+                description: req.body.businessdescription,
+                location: req.body.businessLocation
+        },
+        rep: {
+            name: req.body.adminName,
+            contacts: req.body.adminPhoneNumber,
+            email: req.body.adminEmail,
+            password: req.body.adminPassword,
+            cPassword: req.body.adminConfirmPassword
+        }
+    }
+    
+
+    if (profile.rep.passwod === profile.rep.cPassword) {
+        
+        let sql = 'SELECT * FROM business_profile WHERE b_email = ?'
+
+        connection.query(
+            sql, [profile.business.email], (error, results) => {
+                if (results.length >0) {
+                    let message = 'Business profile with email provided already exists.'
+                    res.render('create-business-profile', {error: true, message: message, profile: profile})
+
+                } else {
+                    bcrypt.hash(profile.rep.password, 10, (error, hash) => {
+                        let sql = 'INSERT INTO business_profile (b_name, b_email, b_tag_line, b_category, b_description, b_location, b_contact_person, b_phone_number, b_email-address, b_password) VALUES (?,?,?,?,?,?,?,?,?,?)'
+                        connection.query(
+                            sql, 
+                            [
+                                profile.business.name,
+                                profile.business.email,
+                                profile.business.tagLine,
+                                profile.business.category,
+                                profile.business.description,
+                                profile.business.location,,
+                                profile.rep.name,
+                                profile.rep.contacts,
+                                profile.rep.email,
+                                hash
+                            ],
+                            (error, results) => {
+                                res.send('/business profile succesfully craeted')
+                            }
+                        )
+                        
+                    })   
+                }
+            }
+        )
+    
+    } else {
+        let message = 'Password and confirm password does not match'
+        res.render('create-business-profile', {error: true, message: message, profile: profile})
+    } 
 })
 
 
