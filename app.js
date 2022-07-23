60d5621ca32ebed34f88c57c3afe4df71ee197c9
voatcodes <valarietila@gmail.com>
2022-07-23 10:51:34 +0300

    added connection to get businesses

--- app.js
+++ app.js
@@ -173,7 +173,17 @@ app.post('/signup', (req, res) => {
 // application homepage
 app.get('/app', (req, res) => {
     if(res.locals.isLoggedIn) {
-        res.render('reviewer-dashboard')
+
+        connection.query(
+            'SELECT * FROM business_profile',
+            (error, results) => {
+
+                res.render('reviewer-dashboard', {businesses: results})
+
+            }
+
+        )
+        
     } else {
         res.redirect('/login')
     }
